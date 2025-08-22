f7921370432c70e9ba95dee9e61788186ce7a8ab
Jasmin Quinatoa <jr.quinatoa@uea.edu.ec>
2025-08-22 18:12:44 -0500

    Creación del proyecto

---
+++ src/main/java/com/mycompany/tarea01/Tarea01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea01;
+
+/**
+ *
+ * @author PC
+ */
+public class Tarea01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Jasmin");
+    }
+}
